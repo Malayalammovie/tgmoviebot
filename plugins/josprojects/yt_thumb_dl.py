3cc93d7c9b2f1d3b77110103b99d87d6abb7473c
Malayalammovie <98443130+Malayalammovie@users.noreply.github.com>
2022-01-28 21:30:06 +0530

    Update yt_thumb_dl.py

--- plugins/josprojects/yt_thumb_dl.py
+++ plugins/josprojects/yt_thumb_dl.py
@@ -40,7 +40,7 @@ async def send_thumbnail(bot, update):
         )
         await update.reply_photo(
             photo=thumbnail,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://telegram.me/josprojects')]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⭕️join Main Channel⭕️', url='https://telegram.me/tamil_rockers_mallu')]]),
             quote=True
         )
         await message.delete()
